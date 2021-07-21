574e62e7e1ab8629b7a41617742795ac5d5ada37
Betzabeth Narvaez <betzynarv@gmail.com>
2021-07-21 02:53:02 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const express = require("express");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+
+// have node serve files for built react app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+
+// handle GEt requests to /api route
+app.get("/api", (req, res) => {
+    res.json({message: "Hello from server!"});
+});
+
+
+// all other GET requests not handled before will return react app
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
